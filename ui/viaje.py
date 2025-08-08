704449de9c09ce38832f833050a9d7405b14a6e2
PaskuRami <ismaelpascualramirez@gmail.com>
2025-08-08 15:13:27 +0200

    Update viaje.py

--- ui/viaje.py
+++ ui/viaje.py
@@ -3,11 +3,40 @@ import pandas as pd
 import plotly.express as px
 #import plotly.express as px 
 
+#Escribir en archivo csv
+def writing_on_csv(dataDict):
+    with open("./data/lista_viaje.csv", mode="w", newline='\n') as file:
+        data = pd.DataFrame(dataDict)
+        data.to_csv(file,index=False,escapechar="\r")
+
+def reading_from_csv():
+    with open("./data/lista_viaje.csv", mode="r") as file:
+        data = pd.read_csv(file)
+        return(data)
+
+#Cambios en las tablas
+def process_changes():
+    editor_state = st.session_state.get("dynamic_editor_travel",{})
+    edited = editor_state.get("edited_rows",{})
+    added = editor_state.get("added_rows",{})
+    deleted = editor_state.get("deleted_rows",{})
 
 def cargar_viaje():
     st.subheader("Lista Viaje")
+    #df = reading_from_csv
     df = pd.read_csv("./data/lista_viaje.csv")
-    st.dataframe(df)
+    #st.dataframe(df)
+
+    #Editable Dataframe con filas dinamicas
+    edited_df = st.data_editor(
+        df,
+        key="dynamic_editor_travel",
+        hide_index= True,
+        use_container_width=True,
+        num_rows="dynamic",
+        on_change=process_changes        
+    )
+    st.button("Guardar", on_click=writing_on_csv, args=[edited_df])
 
     #Contar por pais grafico redondo 
 

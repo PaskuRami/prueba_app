344e6d5d5e3943de097df0f4245737568f8f8714
PaskuRami <ismaelpascualramirez@gmail.com>
2025-08-09 15:58:17 +0200

    Update maleta.py

--- ui/maleta.py
+++ ui/maleta.py
@@ -1,10 +1,54 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+#import plotly.express as px 
 
-def cargar_partidos():
-    st.subheader("Aplicacion")
-    df = pd.read_csv("./data/datosTiendaTecnologiaLatam.csv")
-    st.dataframe(df)
+#Escribir en archivo csv
+def writing_on_csv(dataDict):
+    with open("./data/lista_viaje.csv", mode="w", newline='\n') as file:
+        data = pd.DataFrame(dataDict)
+        data.to_csv(file,index=False,escapechar="\r")
+
+def reading_from_csv():
+    with open("./data/lista_viaje.csv", mode="r") as file:
+        data = pd.read_csv(file)
+        return(data)
+
+#Cambios en las tablas
+def process_changes():
+    editor_state = st.session_state.get("dynamic_editor_travel",{})
+    edited = editor_state.get("edited_rows",{})
+    added = editor_state.get("added_rows",{})
+    deleted = editor_state.get("deleted_rows",{})
+
+def maleta_viaje():
+    st.subheader("Lista Viaje")
+    #df = reading_from_csv
+    df = pd.read_csv("./data/maleta.csv")
+    #st.dataframe(df)
+
+    #Editable Dataframe con filas dinamicas
+    edited_df = st.data_editor(
+        df,
+        column_config={
+        "Estado": st.column_config.CheckboxColumn(
+            "Cargado?",
+            help="Cargado en el coche?",
+            )
+        },
+        key="dynamic_editor_travel",
+        hide_index= True,
+        use_container_width=True,
+        num_rows="dynamic",
+        on_change=process_changes        
+    )
+    st.button("Guardar", on_click=writing_on_csv, args=[edited_df])
+
+    #Contar por pais grafico redondo 
+
+    df_count = df.groupby('proveedor').count().reset_index() 
+    fig = px.pie(df_count, values="producto", names="proveedor", title="Proveedor") 
+    st.plotly_chart(fig)
 
 if __name__ == '__main__':
-    cargar_partidos()
+    maleta_viaje()

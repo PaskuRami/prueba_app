74663136fec8398205b1b9c75f73b3d002bfa873
PaskuRami <ismaelpascualramirez@gmail.com>
2025-09-02 17:09:03 +0200

    Update streamlit_location.py

--- ui/streamlit_location.py
+++ ui/streamlit_location.py
@@ -18,25 +18,29 @@ import pandas as pd
 def glp_maps():
     location_current = streamlit_geolocation()
 
-    st.write("Latitude: " + str(location_current["latitude"]))
-    st.write("Longitude: " + str(location_current["longitude"]))
+    if (location_current != None):
+        st.write("Latitude: " + str(location_current["latitude"]))
+        st.write("Longitude: " + str(location_current["longitude"]))
 
-    m = folium.Map(location=[str(location_current["latitude"]),str(location_current["longitude"])], zoom_start=9)
-    folium.Marker([float(location_current["latitude"]),float(location_current["longitude"])], popup="Casa", tooltip="Ubicacion", icon=folium.Icon(color="blue", icon="star")).add_to(m)
+        m = folium.Map(location=[str(location_current["latitude"]),str(location_current["longitude"])], zoom_start=9)
+        folium.Marker([float(location_current["latitude"]),float(location_current["longitude"])], popup="Casa", tooltip="Ubicacion", icon=folium.Icon(color="blue", icon="star")).add_to(m)
 
-    ## GLP
-    df = pd.read_csv('./data/GLP.csv',sep='|')
+        ## GLP
+        df = pd.read_csv('./data/GLP.csv',sep='|')
 
-    for record in df.values:
-        #print(f"Valores {record[4]}, {record[5]}")
-        folium.Marker(location=[record[4],record[5]],icon=folium.Icon(color="red", icon="flag")).add_to(m)
+        for record in df.values:
+            #print(f"Valores {record[4]}, {record[5]}")
+            folium.Marker(location=[record[4],record[5]],icon=folium.Icon(color="red", icon="flag")).add_to(m)
 
-    #Call to render Folium map in Streamlit
+        #Call to render Folium map in Streamlit
 
-    st_data = st_folium(m, width=725)
+        st_data = st_folium(m, width=725)
+    else:
+        st.write("Pulsa ubicacion por favor")
 
 if __name__ == '__main__':
     glp_maps()
+
 
 
 

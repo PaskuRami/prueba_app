855654a7e15aaab26162012ba73f4e5cd06556fa
PaskuRami <ismaelpascualramirez@gmail.com>
2025-09-02 16:31:58 +0200

    Add files via upload

---
+++ ui/streamlit_location.py
@@ -0,0 +1,40 @@
+##https://github.com/steffanic/streamlit-geolocation
+
+##https://folium.streamlit.app/
+## pip install folium
+##pip install streamlit_folium
+##pip install streamlit_geolocation
+##pip install streamlit
+
+## Estaciones de servicio
+## https://geoportalgasolineras.es/geoportal-instalaciones/DescargarFicheros
+
+import streamlit as st
+from streamlit_geolocation import streamlit_geolocation
+import folium
+from streamlit_folium import st_folium
+import pandas as pd
+
+
+location_current = streamlit_geolocation()
+
+st.write("Latitude: " + str(location_current["latitude"]))
+st.write("Longitude: " + str(location_current["longitude"]))
+
+m = folium.Map(location=[str(location_current["latitude"]),str(location_current["longitude"])], zoom_start=9)
+folium.Marker([float(location_current["latitude"]),float(location_current["longitude"])], popup="Casa", tooltip="Ubicacion", icon=folium.Icon(color="blue", icon="star")).add_to(m)
+
+## GLP
+df = pd.read_csv('GLP.csv',sep='|')
+
+#print(df)
+
+for record in df.values:
+    #print(f"Valores {record[4]}, {record[5]}")
+    folium.Marker(location=[record[4],record[5]],icon=folium.Icon(color="red", icon="flag")).add_to(m)
+
+#Call to render Folium map in Streamlit
+
+st_data = st_folium(m, width=725)
+
+

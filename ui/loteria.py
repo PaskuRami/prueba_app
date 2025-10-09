bb72adf92d6b0faf0e69ed967417b9d3f6d326e0
PaskuRami <ismaelpascualramirez@gmail.com>
2025-10-09 16:29:25 +0200

    Update loteria.py

--- ui/loteria.py
+++ ui/loteria.py
@@ -38,7 +38,7 @@ def cargar_loteria():
     st.button("Guardar", on_click=writing_on_csv, args=[edited_df])
 
     count_precio = df['precio' ].sum()
-    st.title(count_precio)
+    st.title("Recaudacion: "count_precio)
 
     #Contar por unidades_navidad, unidades_nino, precio grafico redondo 
 

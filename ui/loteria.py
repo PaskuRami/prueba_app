fb8a273b0dc36470f68ce15d716219e89b8225ea
PaskuRami <ismaelpascualramirez@gmail.com>
2025-10-12 19:18:56 +0200

    Update loteria.py

--- ui/loteria.py
+++ ui/loteria.py
@@ -42,7 +42,7 @@ def cargar_loteria():
     st.write(f"Loteria Navidad: 150€, Loteria Nino: 75€, Total dinero papeletas: 225€, Total dinero recolectado: {count_precio}€")
 
     fig_count = df.groupby('unidades_navidad').count().reset_index() 
-    fig3 = px.bar(x=["Loteria Navidad", "Loteria Niño"], y=fig_count)
+    fig3 = px.bar(fig_count,"unidades_navidad",color="unidades_navidad")
     st.plotly_chart(fig3)
     
     #Contar por unidades_navidad, unidades_nino, precio grafico redondo 

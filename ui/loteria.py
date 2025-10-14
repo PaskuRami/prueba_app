aa79acc4578dce134b4ebc31816264c415f45857
PaskuRami <ismaelpascualramirez@gmail.com>
2025-10-14 17:31:32 +0200

    Update loteria.py

--- ui/loteria.py
+++ ui/loteria.py
@@ -47,8 +47,8 @@ def cargar_loteria():
     df_sum_lotnino = df['unidades_nino'].sum()
     df_nav = df.groupby('unidades_navidad').sum().reset_index()
     
-    #fig = px.bar(df_nav,x='unidades_navidad',title='Ventas Navidad')
-    #st.plotly_chart(fig)
+    fig = px.bar(df_nav,x='unidades_navidad',title='Ventas Navidad')
+    st.plotly_chart(fig)
 
 if __name__ == '__main__':
     cargar_loteria()
